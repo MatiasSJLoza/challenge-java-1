e0bd163b6fe59703a6102e684e05fae208cfbb9b
Cesley Sousa <ext_cesousa@mercadolibre.com>
2023-05-25 01:31:32 -0300

    se crea la clase Gerente

---
+++ Gerente.java
@@ -0,0 +1,24 @@
+public class Gerente extends Empleado {
+    private double bonificacion;
+
+    public Gerente(String nombre, double salarioBase, int horasTrabajadas, double bonificacion) {
+        super(nombre, salarioBase, horasTrabajadas);
+        this.bonificacion=bonificacion;
+    }
+
+    public double getBonificacion() {
+        return bonificacion;
+    }
+
+    public void setBonificacion(double bonificacion) {
+        this.bonificacion = bonificacion;
+    }
+
+    @Override
+    public double calcularSalarioFinal() {
+        double salarioBase = getSalarioBase();
+        int horasTrabajadas = getHorasTrabajadas();
+        double salarioFinal = salarioBase * horasTrabajadas + bonificacion;
+        return salarioFinal;
+    }
+}
