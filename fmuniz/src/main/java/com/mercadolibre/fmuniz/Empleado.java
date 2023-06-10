76e3ec99cede16d195b68deef583a762fb6f973b
Fernando Gaston Muniz <fernando.muniz@mercadolibre.com>
2023-06-10 01:15:20 -0300

    Proyecto inicial Java

---
+++ fmuniz/src/main/java/com/mercadolibre/fmuniz/Empleado.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mercadolibre.fmuniz;
+
+/**
+ *
+ * @author fmuniz
+ */
+public class Empleado {
+    public static void main (String args[]) {
+        System.out.println("SISTEMA DE GESTION DE EMPLEADOS");
+        
+    }
+    
+}
---
+++ fmuniz/src/main/java/com/mercadolibre/fmuniz/Fmuniz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mercadolibre.fmuniz;
+
+/**
+ *
+ * @author fmuniz
+ */
+public class Fmuniz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
