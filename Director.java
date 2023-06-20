d790dd8d399dc3c4044ff085f5df7ce136841c02
Cesley Sousa <ext_cesousa@mercadolibre.com>
2023-06-20 17:16:45 -0300

    cambio de classe Director

--- Director.java
+++ Director.java
@@ -5,6 +5,7 @@ public class Director extends Empleado {
                     String direccion, String telefono, boolean casado, int hijos) {
         super(nombre, salarioBase, horasTrabajadas, direccion, telefono, casado, hijos);
         this.bonificacion = bonificacion;
+        // O parâmetro "bonificacion" é usado para inicializar o atributo "bonificacion" da classe Director.
     }
 
     public double getBonificacion() {
@@ -14,11 +15,24 @@ public class Director extends Empleado {
     public void setBonificacion(double bonificacion) {
         this.bonificacion = bonificacion;
     }
+
     @Override
     public double calcularSalarioFinal() {
         double salarioBase = getSalarioBase();
         int horasTrabajadas = getHorasTrabajadas();
+        // Os atributos "salarioBase" e "horasTrabajadas" são obtidos através dos métodos getter herdados da classe Empleado.
+
         double salarioFinal = salarioBase * horasTrabajadas + bonificacion;
+        // O cálculo do salário final inclui o salário base multiplicado pelas horas trabalhadas e a bonificação.
+
         return salarioFinal;
+    }
+
+    private int getHorasTrabajadas() {
+        return 0;
+    }
+
+    private double getSalarioBase() {
+        return 0;
     }
 }
