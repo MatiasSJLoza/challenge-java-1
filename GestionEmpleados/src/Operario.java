92f3423da82cf7bf7fc8e50b61aa51e655c8246c
melimvila <marcos.vila@mercadolibre.com>
2023-07-31 01:45:16 -0300

    Update Operario.java

--- GestionEmpleados/src/Operario.java
+++ GestionEmpleados/src/Operario.java
@@ -1,1 +1,10 @@
+public class Operario extends Empleado {
+    public Operario(String nombre, double sueldoBase, int horasTrabajadas) {
+        super(nombre, sueldoBase, horasTrabajadas);
+    }
 
+    @Override
+    public double calcularSueldoFinal() {
+        return getSueldoBase() * getHorasTrabajadas();
+    }
+}
